ecad85ae7f21230d1aefc0bd7ac8aeb630c67b2b
tula18 <ricohen65@gmail.com>
2020-05-26 19:35:10 +0300

    update12

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,6 @@ module.exports = {
         "space-before-function-paren": ["error", "never"],
         "no-trailing-spaces": ["error", { "skipBlankLines": true }],
         "no-multiple-empty-lines": ["error", { "max": 4, "maxEOF": 2 }],
-        
+        "spaced-comment": ["off"]
     }
 };
